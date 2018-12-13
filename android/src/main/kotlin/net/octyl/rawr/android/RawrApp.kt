f6ece5b7f9621702fbc36d7e3d4473392fd1cdaf
Kenzie Togami <ket1999@gmail.com>
2018-12-13 01:35:09 -0800

    Add menu open code

--- android/src/main/kotlin/net/octyl/rawr/android/RawrApp.kt
+++ android/src/main/kotlin/net/octyl/rawr/android/RawrApp.kt
@@ -1,17 +1,22 @@
 package net.octyl.rawr.android
 
-import android.graphics.Color
-import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
 
 class RawrApp : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        drawerLayout = findViewById(R.id.root)
 
         supportActionBar!!.configureRawrApp()
     }
@@ -26,6 +31,14 @@ class RawrApp : AppCompatActivity() {
         setHomeAsUpIndicator(menuIcon)
     }
 
-
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
 
 }
